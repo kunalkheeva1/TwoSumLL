1f767177ef411c66ba27c00dd7c6af6d67daca3a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-22 22:51:48 -0700

    Initial commit

---
+++ src/TwoSumLL.java
@@ -0,0 +1,22 @@
+public class TwoSumLL {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data= data;
+            this.next = null;
+        }
+    }
+    public static Node twoSumLL(Node l1, Node l2){
+        Node head = new Node(0);
+        Node tail = head;
+        int carry =0;
+        int a= 0;
+        int b=0;
+        while()
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
