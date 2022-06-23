475d2e94c8b591f27c1f614e3c6f2dc6d251c321
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-22 22:58:47 -0700

    created a method to add the sum for both reversed linked lists and print that data in ll as well in same(reversed) order.

--- src/TwoSumLL.java
+++ src/TwoSumLL.java
@@ -13,7 +13,23 @@ public class TwoSumLL {
         int carry =0;
         int a= 0;
         int b=0;
-        while()
+        while(l1!=null || l2!=null || carry>0 ){
+            if(l1!=null){
+                l1.data= a;
+                l1=l1.next;
+            }
+            if(l2!=null){
+             l2.data=b;
+             l2 = l2.next;
+            }
+            int sum = (a+b+carry);
+            carry = sum/10;
+            Node temp = new Node(sum%10);
+
+            tail.next =temp;
+            tail = tail.next;
+        }
+        return head.next;
     }
 
     public static void main(String[] args) {
